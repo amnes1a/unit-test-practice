29d08930a8ce8a36e1539507826d00f1dce4abe8
Arturo Zamora <arturo-oz@hotmail.com>
2017-03-13 19:47:41 -0600

    First commit

---
+++ Unit-test-practice/Unit-Test-Hash/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unit_test_practice;
+
+namespace Unit_Test_Hash
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Int64 result = 680131659347;
+            String text = "leepadg";
+
+            Assert.AreEqual(result, Program.Hash(text));
+        }
+    }
+}
---
+++ Unit-test-practice/Unit-test-practice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_test_practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Hash("leepadg"));
+            Console.ReadLine();
+        }
+
+        public static Int64 Hash(String str)
+        {
+            Int64 h = 0;
+            String letters = "";
+            Int64 i = 0;
+            Int64 index = 0;
+            Int64 inc = 0;
+
+            h = 7;
+            letters = "acdegilmnoprstuw";
+
+            for (int x = 0; x < str.Length; x++)
+            {
+                inc = h * 37;
+                index = letters.IndexOf(str[x]);
+                h = inc + index;
+            }
+
+            return h;
+        }
+
+    }
+}
